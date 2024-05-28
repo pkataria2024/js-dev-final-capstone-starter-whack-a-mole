f40bed81159493839f8be7fffafe2a6551a17385
pkataria2024 <pkataria@indeed.com>
2024-05-28 15:15:46 +0530

    Changes done for US-02 - Basic game functionality: Randomness

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ let difficulty = "hard";
  *
  */
 function randomInteger(min, max) {
-  // return Math.floor(Math.random() * (max - min + 1)) + min;
+   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
@@ -39,8 +39,12 @@ function randomInteger(min, max) {
  *
  */
 function setDelay(difficulty) {
-  // TODO: Write your code here.
-  
+  if(difficulty==="easy")
+    return 1500;
+  else if(difficulty==="normal")
+    return 1000;
+  if(difficulty==="hard")
+    return randomInteger(600,1200);
 }
 
 /**
@@ -58,8 +62,13 @@ function setDelay(difficulty) {
  * chooseHole(holes) //> returns one of the 9 holes that you defined
  */
 function chooseHole(holes) {
-  // TODO: Write your code here.
-
+const index = randomInteger(0,8);
+const hole = holes[index];
+if(hole===lastHole) {
+  return chooseHole(holes);
+}
+lastHole = hole;
+return hole;
 }
 
 /**
